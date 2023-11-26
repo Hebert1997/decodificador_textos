169fcdcc06503e00c82e477e3e46e9962b274c0b
Hebert1997 <148573295+Hebert1997@users.noreply.github.com>
2023-11-25 23:34:28 -0300

    projeto finalizado

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
 var input = document.querySelector("#inputTexto")
-var outputText = document.querySelector("#outputTexto")
-var saidaDados = document.querySelector("#saida-dados")
+var imagemSaida = document.querySelector("#saida-dados-imagem")
+var textosSaida = document.querySelector("#saida-dados-textos")
 
 var matrizCode = [
     ["e","enter"],
@@ -10,21 +10,51 @@ var matrizCode = [
     ["u","ufat"],
 ]
 
-function btnCriptografar(){
-    var textoCriptografado = input.value;   
-    criptografar(textoCriptografado);
-}
-
-function criptografar(textoCriptografado){
+function criptografar(stringCriptografada){
     for(i = 0; i < matrizCode.length; i++){
-        if (textoCriptografado.includes(matrizCode[i][0])){ 
+        if (stringCriptografada.includes(matrizCode[i][0])){ 
             //o metodo includes retorna um valor procurado em uma string, nesse caso são os valores da posição 01 no indice matriz code.
-            var novoTexto = textoCriptografado.replaceAll(matrizCode[i][0],matrizCode[i][1]);             
+            stringCriptografada = stringCriptografada.replaceAll(matrizCode[i][0],matrizCode[i][1]); //o metodo replace substitui um trecho da uma string por outra.             
         }
     }
+    return stringCriptografada;
 }
 
-function apagaQuadro(){
-    saidaDados.style.cssText = 
-    "display: none";
+function descriptografar(stringDescriptograda){
+    for (i = 0; i < matrizCode.length; i++){
+        if (stringDescriptograda.includes(matrizCode[i][1])){
+            stringDescriptograda = stringDescriptograda.replaceAll(matrizCode[i][1],matrizCode[i][0]);
+        }
+    }
+    return stringDescriptograda;
+}
+
+function apagaDadosSaida(){
+    imagemSaida.style.cssText = "display: none"    
+}
+
+function btnCriptografar(){    
+    var textoCriptografado = criptografar(input.value);  
+    if (textoCriptografado.match(/[A-Z]/) || textoCriptografado.match(/[áàãâäéèêëíìïîóòöôõúùûü]/)){
+        apagaDadosSaida();
+        textosSaida.value = mensagemErro();
+        input.value="";
+    } else {
+        apagaDadosSaida();
+        textosSaida.value = textoCriptografado;    
+        input.value="";
+    }  
+    }
+    
+
+function btnDescriptografar(){
+    var textoDescriptografado = descriptografar(input.value);   
+    apagaDadosSaida();
+    textosSaida.value = textoDescriptografado;
+    input.value=""; 
+}
+
+function mensagemErro(){
+    var mensagemErro = "Por favor digitar Apenas letras minusculas e sem acento!"
+    return mensagemErro;
 }
